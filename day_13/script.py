3d5cd853e4b54a369bb5ea949721b4657036e6e8
mistermoutan <moutan@protonmail.com>
2023-01-05 13:08:55 +0100

    day 13, part 2

--- day_13/script.py
+++ day_13/script.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from json import loads
 from itertools import zip_longest
 
@@ -8,48 +9,65 @@ def make_same(a, b):
             a = [a]
         if type(b) == int:
             b = [b]
-    return a,b
+    return a, b
 
 
 def rule(a, b, idx):
-    print(f"a {a}, b {b}")
-    if a == b: 
-        return (False,False)
+    if a == b:
+        return (False, False)
     if a is None:
-         return (True,idx)
+        return (True, idx)
     if b is None:
-        return (True,0)
-    a,b = make_same(a, b)
-    print(f"After same {a}, {b}")
+        return (True, 0)
+    a, b = make_same(a, b)
     if type(a) == int:
         if a < b:
-            return (True,idx)
+            return (True, idx)
         if a > b:
-            return (True,0)
+            return (True, 0)
     if type(a) == list:
         iter_ = zip_longest(a, b, fillvalue=None)
-        c,d = next(iter_)
+        c, d = next(iter_)
         done = False
         while not done:
-            done, add = rule(c,d,idx)
+            done, add = rule(c, d, idx)
             if done:
                 return done, add
             try:
-                c,d = next(iter_)
+                c, d = next(iter_)
             except StopIteration:
-                return False,False
+                return False, False
+    # return (False,False)
+
 
 def part_1(pairs):
     res = 0
     for idx, pair in enumerate(pairs):
         for a, b in zip_longest(pair[0], pair[1], fillvalue=None):
-            print(f"\n PAIRS: \n {pair[0]} \n {pair[1]}")
-            pair_analysis_done, add = rule(a, b, idx+1)
+            pair_analysis_done, add = rule(a, b, idx + 1)
             if pair_analysis_done:
                 res += add
                 break
     return res
 
+
+def part_2(pairs):
+    all_lines = [i for pair in pairs for i in pair]
+    all_lines.append([[2]])
+    all_lines.append([[6]])
+    is_higher_than = defaultdict(int)
+    for idx, line in enumerate(all_lines):
+        for idx2, line2 in enumerate(all_lines):
+            if idx == idx2:
+                continue
+            for a, b in zip_longest(line, line2, fillvalue=None):
+                pair_analysis_done, add = rule(a, b, 10)
+                if pair_analysis_done:
+                    if add == 0:
+                        is_higher_than[str(line)] += 1
+                    break
+
+    return ((is_higher_than["[[2]]"] +1) * (1+is_higher_than["[[6]]"]))
 
 
 with open("input.txt") as f:
@@ -64,3 +82,4 @@ with open("input.txt") as f:
             pair = []
     pairs.append(pair)
     print(part_1(pairs))
+    print(part_2(pairs))
